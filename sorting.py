0a2fd2f200f25ac4d87b645a4676e0a21c0612a8
Agnes Malarzova <278115@vutbr.cz>
2026-04-22 09:43:27 +0200

    bubble_sort

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -22,49 +23,37 @@ def selection_sort(values):
 
     return values
 
+def bubble_sort(numbers):
+    numbers = numbers.copy()
+    delka = len(numbers)
+
+    plt.ion()
+    plt.show()
+
+    for i in range(delka):
+        for j in range(0, delka - i - 1):
+            index_highlight1 = j
+            index_highlight2 = j + 1
+            colors = ["steelblue"] * len(numbers)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(numbers)), numbers, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+
+            if numbers  [j] > numbers[j+1]:
+                numbers[j], numbers [j+1] = numbers [j+1], numbers[j]
+
+    plt.ioff()
+    plt.show()
+
+    return numbers
+
 
 if __name__ == "__main__":
     values = random_numbers(10)  # 10 čísel v rozsahu 0–100
+    numbers = random_numbers(20)
    # print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
     selection_sort(values)
-
-# import random
-#
-#
-# def random_numbers(count):
-#     return [random.randint(1, 100) for _ in range(count)]
-#
-#
-# def selection_sort(seznam):
-#     data = seznam.copy()
-#     n = len(data)
-#
-#     for i in range(n):
-#         min_idx = i
-#         for j in range(i + 1, n):
-#             if data[j] < data[min_idx]:
-#                 min_idx = j
-#
-#
-#         data[i], data[min_idx] = data[min_idx], data[i]
-#
-#     return data
-#
-#
-# def main():
-#
-#     cisla = [5, 1, 4, 2, 8]
-#     print("Původní:", cisla)
-#     print("Seřazený:", selection_sort(cisla))
-#
-#
-#     nahodna = random_numbers(20)
-#     print("\nNáhodná čísla:", nahodna)
-#     print("Seřazená náhodná:", selection_sort(nahodna))
-#
-#
-# if __name__ == "__main__":
-#     main()
-#
-#
-
+    bubble_sort(numbers)
